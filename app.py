2231e324ae28c3c7467d3c48f1ad08698cf05bf5
T125055 <t125055@ed.sus.ac.jp>
2026-01-31 03:21:33 +0900

    1st commit

--- app.py
+++ app.py
@@ -1,38 +1,33 @@
 import streamlit as st
 import pandas as pd
 
-st.title('自然公園の公園数と年間利用者数')
+st.title('都道府県別にみた年別死亡数')
 
-df = pd.read_csv('park.csv')
+df = pd.read_csv('people.csv')
 
-# 年次を数値化
-df['年次'] = pd.to_numeric(df['年次'], errors='coerce')
-df = df.dropna(subset=['年次'])
+df_long = df.melt(
+    id_vars='都道府県',
+    var_name='年',
+    value_name='死亡数')
 
-# df が空なら止める
-if df.empty:
-    st.error('年次データが読み込めません。CSVを確認してください。')
-    st.stop()
-
-df['年次'] = df['年次'].astype(int)
+df_long['年'] = df_long['年'].astype(int)
 
 with st.sidebar:
-    branch = st.multiselect('公園分類を選択してください（複数選択可）',
-                            df['公園分類'].unique())
-    # 
-    year = st.slider('年を選択してください',
-                     min_value=int(df['年次'].min()),
-                     max_value=int(df['年次'].max()),
-                     value=int(df['年次'].min()),
-                     step=1)
+    branch = st.multiselect('都道府県を選択してください（複数選択可）',
+                            df['都道府県'].unique())
+    
+    year = st.slider(label='年を選択してください',
+                        min_value= 1975,
+                        max_value= 2024,
+                        value=2000 )
 
-#     
-if branch:
-    df = df[df['公園分類'].isin(branch)]
+df = df_long.copy()
 
-df = df[df['年次']==year]
-df.drop('年次',axis=1,inplace=True)
-df.set_index('公園分類',inplace=True)
+df = df[df['都道府県'].isin(branch)]
+df = df[df['年'] == year]
+
+df.drop('年', axis=1, inplace=True)
+df.set_index('都道府県', inplace=True)
 
 st.dataframe(df, width=800, height=200)
 st.line_chart(df)
